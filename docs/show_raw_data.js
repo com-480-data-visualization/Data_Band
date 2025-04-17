75c22596d9fd95f8fb578fd2cb5b222ad18a675e
TheTorf <jean-daniel.rouveyrol@epfl.ch>
2025-04-17 18:43:04 +0200

    raw_data_sample

--- docs/show_raw_data.js
+++ docs/show_raw_data.js
@@ -1,47 +1,37 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
-d3.csv("https://raw.githubusercontent.com/thetorf/files/main/test.csv").then(
-    function(data) {
+d3.csv("https://raw.githubusercontent.com/thetorf/files/main/fraud_dataset.csv").then(
+    function(d) {
         
+        let data = d.slice(0,10)
+
         for (let index = 0; index < data.length; index++){
             const element = data[index]
             console.log(element)
         }
-        
+
         console.log(d3.select("div.content-box").node())
 
         const table = d3.select("div.content-box")
-            .append("table")
-            .style("border-collapse", "collapse")
-            .style("margin", "20px")
-            .style("margin-left", "auto")
-            .style("margin-right", "auto");
+            .append("table");
 
         const thead = table.append("thead");
         const tbody = table.append("tbody");
 
         thead.append("tr")
             .selectAll("th")
-            .data(data.columns)
+            .data(d.columns)
             .enter()
             .append("th")
-            .text(d => d)
-            .style("border", "1px solid #ccc")
-            .style("padding", "8px")
-            .style("background-color", "#3b3b3b")
-            .style("color", "#ffffff");
+            .text(d => d);
 
         data.forEach(row => {
             const tr = tbody.append("tr");
             Object.entries(row).forEach(value => {
                 tr.append("td")
-                .text(value[1])
-                .style("border", "1px solid #ccc")
-                .style("padding", "8px")
-                .style("background-color", '#f3f4f6');
+                    .text(value[1]);
             });
         });
-
-
+        
     }
 );
